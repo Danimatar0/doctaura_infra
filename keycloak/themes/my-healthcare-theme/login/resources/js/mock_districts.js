a7c1bf99535c266282ea8d6ff7b868086adf040d
Danimatar0 <matardani2@gmail.com>
2025-12-13 19:54:11 +0200

    Fix mock localities mappings to districts

--- keycloak/themes/my-healthcare-theme/login/resources/js/mock_districts.js
+++ keycloak/themes/my-healthcare-theme/login/resources/js/mock_districts.js
@@ -1,9 +1,12 @@
 // Mock Districts Data
 export const MOCK_DISTRICTS = [
-  { id: 202, name: "Aley", type: "District", parentId: 2, parentName: "Mount Lebanon" },
+  // Beirut Governorate (id: 1)
+  { id: 101, name: "Beirut", type: "District", parentId: 1, parentName: "Beirut" },
+  // Mount Lebanon Governorate (id: 2)
   { id: 201, name: "Baabda", type: "District", parentId: 2, parentName: "Mount Lebanon" },
+  { id: 202, name: "Aley", type: "District", parentId: 2, parentName: "Mount Lebanon" },
   { id: 203, name: "Chouf", type: "District", parentId: 2, parentName: "Mount Lebanon" },
-  { id: 206, name: "Jbeil", type: "District", parentId: 2, parentName: "Mount Lebanon" },
   { id: 204, name: "Keserwan", type: "District", parentId: 2, parentName: "Mount Lebanon" },
-  { id: 205, name: "Matn", type: "District", parentId: 2, parentName: "Mount Lebanon" }
+  { id: 205, name: "Matn", type: "District", parentId: 2, parentName: "Mount Lebanon" },
+  { id: 206, name: "Jbeil", type: "District", parentId: 2, parentName: "Mount Lebanon" }
 ];
--- keycloak/themes/my-healthcare-theme/login/resources/js/mock_localities.js
+++ keycloak/themes/my-healthcare-theme/login/resources/js/mock_localities.js
@@ -1,18 +1,52 @@
 // Mock Localities Data
 export const MOCK_LOCALITIES = [
+  // Beirut District (id: 101)
   { id: 10101, name: "Achrafieh", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10114, name: "Ain el-Mreisseh", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10111, name: "Bachoura", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10107, name: "Badaro", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10113, name: "Downtown Beirut", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10106, name: "Gemmayze", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
   { id: 10102, name: "Hamra", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10105, name: "Mar Mikhael", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10108, name: "Mazraa", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10115, name: "Minet el-Hosn", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10109, name: "Msaytbeh", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10104, name: "Ras Beirut", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10112, name: "Saifi", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
   { id: 10103, name: "Verdun", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
-  { id: 10110, name: "Zuqaq al-Blat", type: "Neighborhood", parentId: 101, parentName: "Beirut" }
+  { id: 10104, name: "Ras Beirut", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10105, name: "Mar Mikhael", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10106, name: "Gemmayze", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10107, name: "Badaro", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10108, name: "Mazraa", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10109, name: "Msaytbeh", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10110, name: "Zuqaq al-Blat", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10111, name: "Bachoura", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10112, name: "Saifi", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10113, name: "Downtown Beirut", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10114, name: "Ain el-Mreisseh", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  { id: 10115, name: "Minet el-Hosn", type: "Neighborhood", parentId: 101, parentName: "Beirut" },
+  // Baabda District (id: 201)
+  { id: 20101, name: "Baabda", type: "Locality", parentId: 201, parentName: "Baabda" },
+  { id: 20102, name: "Hazmieh", type: "Locality", parentId: 201, parentName: "Baabda" },
+  { id: 20103, name: "Hadath", type: "Locality", parentId: 201, parentName: "Baabda" },
+  { id: 20104, name: "Furn el-Chebbak", type: "Locality", parentId: 201, parentName: "Baabda" },
+  { id: 20105, name: "Chiyah", type: "Locality", parentId: 201, parentName: "Baabda" },
+  // Aley District (id: 202)
+  { id: 20201, name: "Aley", type: "Locality", parentId: 202, parentName: "Aley" },
+  { id: 20202, name: "Bhamdoun", type: "Locality", parentId: 202, parentName: "Aley" },
+  { id: 20203, name: "Sofar", type: "Locality", parentId: 202, parentName: "Aley" },
+  { id: 20204, name: "Ain Anoub", type: "Locality", parentId: 202, parentName: "Aley" },
+  // Chouf District (id: 203)
+  { id: 20301, name: "Beiteddine", type: "Locality", parentId: 203, parentName: "Chouf" },
+  { id: 20302, name: "Deir el-Qamar", type: "Locality", parentId: 203, parentName: "Chouf" },
+  { id: 20303, name: "Baakline", type: "Locality", parentId: 203, parentName: "Chouf" },
+  { id: 20304, name: "Moukhtara", type: "Locality", parentId: 203, parentName: "Chouf" },
+  // Keserwan District (id: 204)
+  { id: 20401, name: "Jounieh", type: "Locality", parentId: 204, parentName: "Keserwan" },
+  { id: 20402, name: "Harissa", type: "Locality", parentId: 204, parentName: "Keserwan" },
+  { id: 20403, name: "Zouk Mosbeh", type: "Locality", parentId: 204, parentName: "Keserwan" },
+  { id: 20404, name: "Ghazir", type: "Locality", parentId: 204, parentName: "Keserwan" },
+  { id: 20405, name: "Faraya", type: "Locality", parentId: 204, parentName: "Keserwan" },
+  // Matn District (id: 205)
+  { id: 20501, name: "Jdeideh", type: "Locality", parentId: 205, parentName: "Matn" },
+  { id: 20502, name: "Antelias", type: "Locality", parentId: 205, parentName: "Matn" },
+  { id: 20503, name: "Dbayeh", type: "Locality", parentId: 205, parentName: "Matn" },
+  { id: 20504, name: "Beit Mery", type: "Locality", parentId: 205, parentName: "Matn" },
+  { id: 20505, name: "Broummana", type: "Locality", parentId: 205, parentName: "Matn" },
+  // Jbeil District (id: 206)
+  { id: 20601, name: "Byblos", type: "Locality", parentId: 206, parentName: "Jbeil" },
+  { id: 20602, name: "Amchit", type: "Locality", parentId: 206, parentName: "Jbeil" },
+  { id: 20603, name: "Kartaba", type: "Locality", parentId: 206, parentName: "Jbeil" },
+  { id: 20604, name: "Annaya", type: "Locality", parentId: 206, parentName: "Jbeil" }
 ];
